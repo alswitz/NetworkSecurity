f5ae0c0312fb960a60ea24fd487b19e126f3f00a
alswitz <alexanderswitz09@gmail.com>
2015-09-15 16:41:43 -0400

    Added first version of script.

---
+++ Assignment1/Part1/script.sh
@@ -0,0 +1,7 @@
+!/bash/shell
+
+grep "[Cc]" words.txt > ~NetworkSecurity/Assignment1/Part1/c/
+grep "[Dd]" words.txt > ~NetworkSecurity/Assignment1/Part1/d/
+grep "[Zz]" words.txt > ~NetworkSecurity/Assignment1/Part1/z/
+rm -f words.txt
+ls -l
