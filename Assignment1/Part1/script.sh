5e9ee5aee5e143e7fe5614a29071def960336508
alswitz <alexanderswitz09@gmail.com>
2015-09-15 20:57:24 -0400

    Added mkdir commands.

--- Assignment1/Part1/script.sh
+++ Assignment1/Part1/script.sh
@@ -1,5 +1,9 @@
 !/bash/shell
 
+mkdir c
+mkdir d
+mkdir z
+
 cat words.txt | grep "[Cc]" words.txt > c/sortedwords.txt
 cat words.txt | grep "[Dd]" words.txt > d/sortedwords.txt
 cat words.txt | grep "[Zz]" words.txt > z/sortedwords.txt
