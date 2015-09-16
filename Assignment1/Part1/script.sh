c808ed126dc1e45eae6be886b01725b9a120f0b6
alswitz <alexanderswitz09@gmail.com>
2015-09-15 22:11:10 -0400

    Changed script code.

--- Assignment1/Part1/script.sh
+++ Assignment1/Part1/script.sh
@@ -1,11 +1,9 @@
-!/bash/shell
-
 mkdir c
 mkdir d
 mkdir z
 
-cat words.txt | grep "[Cc]" words.txt > c/sortedwords.txt
-cat words.txt | grep "[Dd]" words.txt > d/sortedwords.txt
-cat words.txt |grep "[Zz]" words.txt > z/sortedwords.txt
+cat words.txt | grep "^[Cc]" words.txt > c/sortedwords.txt
+cat words.txt | grep "^[Dd]" words.txt > d/sortedwords.txt
+cat words.txt | grep "^[Zz]" words.txt > z/sortedwords.txt
 rm -f words.txt
 ls -l
