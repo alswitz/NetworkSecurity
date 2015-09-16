40581c72a361184f51388b179e1499857e0a48b3
alswitz <alexanderswitz09@gmail.com>
2015-09-15 21:35:58 -0400

    Trying it a different way.

--- Assignment1/Part1/script.sh
+++ Assignment1/Part1/script.sh
@@ -3,9 +3,8 @@
 mkdir c
 mkdir d
 mkdir z
-
-cat words.txt | grep "[Cc]" words.txt > c/sortedwords.txt
-cat words.txt | grep "[Dd]" words.txt > d/sortedwords.txt
-cat words.txt | grep "[Zz]" words.txt > z/sortedwords.txt
+grep "[Cc]" words.txt > c/sortedwords.txt
+grep "[Dd]" words.txt > d/sortedwords.txt
+grep "[Zz]" words.txt > z/sortedwords.txt
 rm -f words.txt
 ls -l
