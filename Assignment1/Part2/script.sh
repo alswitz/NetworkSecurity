80e037edb732a15bafb5a8c395be1a84f25cdf70
alswitz <alexanderswitz09@gmail.com>
2015-09-15 23:11:37 -0400

    Draft 2.

--- Assignment1/Part2/script.sh
+++ Assignment1/Part2/script.sh
@@ -1,4 +1,21 @@
-for char in {z..a}; do
+cp -r ~/NetworkSecurity/Assignment1/Part1/c .
+cp -r ~/NetworkSecurity/Assignment1/Part1/d .
+cp -r ~/NetworkSecurity/Assignment1/Part1/z .
+
+for char in {z..a}; do 
 	mkdir $char
+done
+
+for char {z..a}; do
+	grep $char/sortedwords.txt >> reversedwords.txt
+done
+
+#grep "^[zZ]" z/sortedwords.txt > reversedwords.txt
+#grep "^[Dd]" d/sortedwords.txt >> reversedwords.txt
+#grep "^[Cc]" c/sortedwords.txt >> reversedwords.txt
+
+
+for char in {z..a}; do
+	rm -rf $char
 done
 
