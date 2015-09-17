8d62d51ddf516477cf3f0c70065cc417322dbaa8
alswitz <alexanderswitz09@gmail.com>
2015-09-17 11:37:58 -0400

    Re-added words.txt

--- Assignment1/Part1/part1.sh
+++ Assignment1/Part1/part1.sh
@@ -1,9 +1,13 @@
-mkdir c
-mkdir d
-mkdir z
+for char in {a..z}; do
+        mkdir $char
+done
 
-cat words.txt | grep "^[Cc]" words.txt > c/sortedwords.txt
-cat words.txt | grep "^[Dd]" words.txt > d/sortedwords.txt
-cat words.txt | grep "^[Zz]" words.txt > z/sortedwords.txt
-rm -f words.txt
-ls -l
+for char in {a..z}; do
+	cat words.txt | grep "^[$char]"
+done
+ 
+#cat words.txt | grep "^[Cc]" words.txt > c/sortedwords.txt
+#cat words.txt | grep "^[Dd]" words.txt > d/sortedwords.txt
+#cat words.txt | grep "^[Zz]" words.txt > z/sortedwords.txt
+#rm -f words.txt
+#ls -l
