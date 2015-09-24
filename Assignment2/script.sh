506c08b1fcf78d04d7f8625cf51afd1f3f945b2c
alswitz <alexanderswitz09@gmail.com>
2015-09-24 19:29:32 -0400

    Some progress.

--- Assignment2/script.sh
+++ Assignment2/script.sh
@@ -1,1 +1,5 @@
 #This is the beginning.
+#sudo nmap -sP 192.168.0.0/24 >> log_file.txt
+grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" log_file.txt >> ip_addresses.txt
+grep -i "[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}" log_file.txt >> mac_addresses.txt
+
