9b13582521ebc79643f2eff0aa7476d01b29f913
alswitz <alexanderswitz09@gmail.com>
2015-09-27 19:57:43 -0400

    Added graph for IP Addresses.

---
+++ Assignment2/ip_plot.sh
@@ -0,0 +1,13 @@
+set xlabel "IP Addresses Found"
+set ylabel "Frequency"
+set style fill solid
+set style data boxes
+set boxwidth 0.1
+set offset graph 0.1, graph 0.1
+set term png
+set xtics rotate by -45
+set output "ip_graph.png"
+set xr [1:10]
+set yr [1:10]
+plot "sorted_ips.dat" using 0:1:xtic(2) with boxes;
+
--- Assignment2/script.sh
+++ Assignment2/script.sh
@@ -1,13 +1,14 @@
 #This is the beginning.
+#!/usr/bin/env gnuplot
 echo Running network analysis tool...
 #sudo nmap -sP 192.168.0.0/24 >> log_file.txt
 grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" log_file.txt >> ip_addresses.txt
-sort ip_addresses.txt | uniq -c > sorted_ips.txt
+sort ip_addresses.txt | uniq -c > sorted_ips.dat 
 grep -i "[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}" log_file.txt >> mac_addresses.txt
-sort mac_addresses.txt | uniq -c > sorted_macs.txt
-#watch -n 15 ./script.sh
-mac_numbers=$(wc -l < sorted_macs.txt)
+sort mac_addresses.txt | uniq -c > sorted_macs.dat
+mac_numbers=$(wc -l < sorted_macs.dat)
 echo $mac_numbers Unique Mac Addresses found.
-ip_numbers=$(wc -l < sorted_ips.txt)
+ip_numbers=$(wc -l < sorted_ips.dat)
 echo $ip_numbers Unique IP Addresses found.
-
+gnuplot ./ip_plot.sh
+display ip_graph.png
