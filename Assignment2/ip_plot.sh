fe9a76afb29e4fb804c5b85679f0f9c3cfec06e6
alswitz <alexanderswitz09@gmail.com>
2015-09-27 20:01:04 -0400

    Changed X/Y Value.

--- Assignment2/ip_plot.sh
+++ Assignment2/ip_plot.sh
@@ -7,7 +7,7 @@ set offset graph 0.1, graph 0.1
 set term png
 set xtics rotate by -45
 set output "ip_graph.png"
-set xr [1:10]
-set yr [1:10]
+set xr [0:10]
+set yr [0:10]
 plot "sorted_ips.dat" using 0:1:xtic(2) with boxes;
 
